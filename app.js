28961cd65fe5ab58f4cf80039b9a334214e04d79
milanzagorac <zagoracm@gmail.com>
2020-12-07 21:22:24 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+generateJoke()
+
+jokeBtn.addEventListener('click', () => {
+    generateJoke();
+});
+
+
+function generateJoke() {
+    const config = {
+        headers: {
+            'Accept': 'application/json',
+        }
+    };
+
+    fetch('https://icanhazdadjoke.com', config)
+        .then(response => response.json())
+        .then(data => {
+            jokeEl.innerHTML = data.joke;
+        });
+
+};
+
+
+//async await open -->  more popular and possiby the better solution.
+// async function generateJoke() {
+//     const config = {
+//         headers: {
+//             'Accept': 'application/json',
+//         }
+//     };
+
+//     const res = await fetch('https://icanhazdadjoke.com', config)
+
+//     const data = await res.json();
+//     jokeEl.innerHTML = data.joke;
+        
+// };
